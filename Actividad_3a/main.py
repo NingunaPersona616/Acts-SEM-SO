60ee26ae1f104e43332a9afdfdb56bbb01254d2c
Brandon Hernandez Ledezma <brandon.hernandez5150@alumnos.udg.mx>
2022-08-28 14:08:57 -0500

    Bug with the startButton Fix

--- Actividad_3a/main.py
+++ Actividad_3a/main.py
@@ -4,7 +4,10 @@ from PyQt5.QtWidgets import QApplication
 from multiProgramming import *
 
 def startButton():#el boton llamará a la funcion que inicia el proceso de simulacion de la multiprogramacion
+    widget.startButton.setEnabled(False)#Se desactiva el boton para no bugear la interfaz
     multiprogramming(widget)#mandamos como parametro a la interfaz para que puedan ser manipulados sus elementos en las funciones
+    QtTest.QTest.qWait(10100)
+    widget.startButton.setEnabled(True)#al finalizar el procesamiento volvera a estar habilitado el boton
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
