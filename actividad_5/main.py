0fc3f63ddcada68ee695b19ab232caab1122aebb
Brandon Hernandez Ledezma <brandon.hernandez5150@alumnos.udg.mx>
2022-09-29 17:05:46 -0500

    Added RR funcionality

--- actividad_5/main.py
+++ actividad_5/main.py
@@ -4,9 +4,11 @@ from PyQt5.QtWidgets import QApplication
 from threadClass import *
 from PyQt5 import QtCore
 
+CantProcesos = 3
+
 def restartBars():
-    for i in bars:
-        i.setValue(0)
+    for Bar in bars:
+        Bar.setValue(0)
 
 #FCFS ta hecho con una cola de indices(supestamente) y con barras (pero igual se puede usar los hilos directamente)
 def FCFS():
@@ -29,6 +31,54 @@ def FCFS():
         
     widget.startButton.setEnabled(True)
 
+def RR():
+    restartBars()
+    widget.startButton_2.setEnabled(False)
+
+    colaProcesos=[{"index": 0, "status": "init"}, {"index": 1, "status": "init"}, {"index": 2, "status": "init"},]
+    quantum = 0
+
+    while(len(colaProcesos)>0):
+        QtTest.QTest.qWait(400) #Espera lo que espera la barra de carga o se bugea pq trabajan en contextos diferentes y se desincronizan
+        #print('Thread: ', index, 'value: ', bars[index].value())
+        app.processEvents() #funcion pa que no se freezie la GUI con el while
+        Proceso = colaProcesos[0] #Toma el tope de la cola
+        index = Proceso['index']
+        status = Proceso['status']
+        print(index, status)
+
+        print('Thread: ', index, 'value: ', bars[index].value())
+
+        if(bars[index].value() == 100):
+            stopThread(widget, index)
+            colaProcesos[0]['status'] = "finished"
+            colaProcesos.pop(0)            
+
+        elif(status == "init"):
+            startThread(widget, index)
+            colaProcesos[0]['status'] = "running"
+            status = "running"
+
+        elif(status == "stopped"):
+            startThread(widget, index)
+            colaProcesos[0]['status'] = "running"
+            status == "running"
+
+        elif((bars[index].value() % 10) == 0):
+            stopThread(widget, index)
+            colaProcesos[0]['status'] = "stopped"
+            status = "stopped"
+            Aux = colaProcesos.pop(0)
+            colaProcesos.append(Aux)
+            quantum = 0
+
+        elif(status == "running"):
+            print("jalando")
+
+
+    widget.startButton_2.setEnabled(True)
+        
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
@@ -41,4 +91,5 @@ if __name__ == "__main__":
 
     widget.thread={}
     widget.startButton.clicked.connect(FCFS)
+    widget.startButton_2.clicked.connect(RR)
     sys.exit(app.exec())
