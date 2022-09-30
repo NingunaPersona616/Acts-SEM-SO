c4bc7d1cc35fcc7e99487a4ae136bc2e61c844ac
Brandon Hernandez <brandon.hernandez5150@alumnos.udg.mx>
2022-09-29 22:07:12 -0500

    Added comments to main.py

--- actividad_5/main.py
+++ actividad_5/main.py
@@ -35,44 +35,40 @@ def RR():
     restartBars()
     widget.startButton_2.setEnabled(False)
 
-    colaProcesos=[{"index": 0, "status": "init"}, {"index": 1, "status": "init"}, {"index": 2, "status": "init"},]
+    colaProcesos=[{"index": 0, "status": "init"}, {"index": 1, "status": "init"}, {"index": 2, "status": "init"}]
     quantum = 0
 
-    while(len(colaProcesos)>0):
+    while(len(colaProcesos)>0): #Mientras haya un proceso en la cola
         QtTest.QTest.qWait(400) #Espera lo que espera la barra de carga o se bugea pq trabajan en contextos diferentes y se desincronizan
-        #print('Thread: ', index, 'value: ', bars[index].value())
+
         app.processEvents() #funcion pa que no se freezie la GUI con el while
-        Proceso = colaProcesos[0] #Toma el tope de la cola
-        index = Proceso['index']
-        status = Proceso['status']
-        print(index, status)
+
+        #Toma el tope de la cola
+        index = colaProcesos[0]['index']
+        print(index, colaProcesos[0]['status'])
 
         print('Thread: ', index, 'value: ', bars[index].value())
 
-        if(bars[index].value() == 100):
+        if(bars[index].value() == 100): #Si el proceso ya esta al 100% lo detiene y lo borra de la cola
             stopThread(widget, index)
             colaProcesos[0]['status'] = "finished"
             colaProcesos.pop(0)            
 
-        elif(status == "init"):
+        elif(colaProcesos[0]['status'] == "init"):  #Si es un proceso nuevo pues lo inicia
             startThread(widget, index)
             colaProcesos[0]['status'] = "running"
-            status = "running"
 
-        elif(status == "stopped"):
+        elif(colaProcesos[0]['status'] == "stopped"):   #Si el proceso esta en pausa lo inicia
             startThread(widget, index)
             colaProcesos[0]['status'] = "running"
-            status == "running"
 
-        elif((bars[index].value() % 10) == 0):
+        elif((bars[index].value() % 10) == 0):  #Si el proceso alcanzo el quantum de ejecucion
             stopThread(widget, index)
-            colaProcesos[0]['status'] = "stopped"
-            status = "stopped"
-            Aux = colaProcesos.pop(0)
-            colaProcesos.append(Aux)
-            quantum = 0
+            colaProcesos[0]['status'] = "stopped"   #Detiene el proceso
+            Aux = colaProcesos.pop(0)   #Lo kickea de la cola
+            colaProcesos.append(Aux)    #Y lo vuelve a encolar al final
 
-        elif(status == "running"):
+        elif(colaProcesos[0]['status'] == "running"):   #Si esta el proceso se esta corriendo, pues sigue corriendo
             print("jalando")
 
 
