66152433adfa6f5ecc80b1e3b3d9026f6e835a20
SenpaiSuchil <chompiram11@gmail.com>
2022-10-21 01:01:42 -0500

    implementacion main v2

--- actividad_9/main.py
+++ actividad_9/main.py
@@ -4,10 +4,19 @@ from PyQt5.QtWidgets import QApplication
 from PyQt5 import QtCore
 #widget.label_1.setStyleSheet("background-color: gray")
 
+def setDefault():
+    cont = 0
+    for i in vslot:
+        i.setStyleSheet("background-color: gray")
+        i.setText("             x")
+
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     widget = MainWindow()
     widget.show()
-    
+    vslot=[widget.slot_0, widget.slot_1, widget.slot_2, widget.slot_3, widget.slot_4, widget.slot_5, widget.slot_6, widget.slot_7, widget.slot_8, widget.slot_9, widget.slot_10, widget.slot_11]
+    setDefault()
     sys.exit(app.exec())
--- actividad_9/mainwindow.py
+++ actividad_9/mainwindow.py
@@ -9,6 +9,4 @@ class MainWindow(QMainWindow):
 
     def taskManager(self, counter):#processFunction recibira como parametro un contador que sera el que se cargará en la barra de progreso
             cnt=counter#lo igualamos a una variable local
-            index=self.sender().index#cachamos el index del thread para saber que barra llenar haciendo match entre barra e hilo
-            #esta funcion será llamada varias veces mediante señales y es la que generará la carga visual de las barras segun el hilo que emita la señal
             self.bar_0.setValue(cnt)
